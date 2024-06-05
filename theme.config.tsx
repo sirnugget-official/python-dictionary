6ecdd662d5255809e5ebaf04987f5305fc18c672
sirnugget-official <151244489+sirnugget-official@users.noreply.github.com>
2024-06-06 09:23:57 +1000

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Python Dictionary</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/sirnugget-official',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/sirnugget-official',
   footer: {
     text: 'Nextra Docs Template',
   },
