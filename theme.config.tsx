60ac54dc13a52a7bbb28833e7d6ca18604122696
sirnugget-official <151244489+sirnugget-official@users.noreply.github.com>
2024-06-06 17:36:24 +1000

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/sirnugget-official',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Made with 💖 by Mayson Reader',
   },
 }
 
