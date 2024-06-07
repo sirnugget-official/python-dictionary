025f5f78cef855850762f4bc00b3b0817618ea77
sirnugget-official <151244489+sirnugget-official@users.noreply.github.com>
2024-06-07 21:26:49 +1000

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Python Dictionary</span>,
   project: {
-    link: 'https://github.com/sirnugget-official',
+    link: 'https://github.com/sirnugget-official/python-dictionary/',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/sirnugget-official',
+  docsRepositoryBase: 'https://github.com/sirnugget-official/python-dictionary/',
   footer: {
     text: 'Made with 💖 by Mayson Reader',
   },
